5b4e160170eb437a604de4502521a69899c5de27
wcsteve <wcsteve9@gmail.com>
2018-07-21 21:22:05 -0700

    Update algos

--- hashmap_lru/project/firstname_lastname/lib/p01_int_set.rb
+++ hashmap_lru/project/firstname_lastname/lib/p01_int_set.rb
@@ -1,22 +1,34 @@
+require 'byebug'
+
 class MaxIntSet
+  attr_reader :store, :max
+
   def initialize(max)
+    @store =  Array.new(max, false)
+    @max = max
   end
 
   def insert(num)
+    raise "Out of bounds" unless is_valid?(num)
+    @store[num] = true unless self.include?(num)
   end
 
   def remove(num)
+    @store[num] = false if self.include?(num)
   end
 
   def include?(num)
+    @store[num]
   end
 
   private
 
   def is_valid?(num)
+    @max >= num && num >= 0
   end
 
   def validate!(num)
+    @store[num] = true
   end
 end
 
@@ -27,18 +39,25 @@ class IntSet
   end
 
   def insert(num)
+    bucket = self[num]
+    bucket.push(num) unless include?(num)
   end
 
   def remove(num)
+    bucket = self[num]
+    bucket.delete(num)
   end
 
   def include?(num)
+    bucket = self[num]
+    bucket.include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -67,6 +86,7 @@ class ResizingIntSet
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
