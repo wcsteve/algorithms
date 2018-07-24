128cafa6a1207797495b3ce92551122b473a04c2
wcsteve <wcsteve9@gmail.com>
2018-07-23 21:12:31 -0700

    Add Dynamic Array

--- arrays/project/firstname_lastname/lib/dynamic_array.rb
+++ arrays/project/firstname_lastname/lib/dynamic_array.rb
@@ -4,23 +4,34 @@ class DynamicArray
   attr_reader :length
 
   def initialize
+    @store = StaticArray.new
+    @capacity = 8
+    @length = @store.length
   end
 
   # O(1)
   def [](index)
+    self.check_index(index)
   end
 
   # O(1)
   def []=(index, value)
+    @store[index] = value
   end
 
   # O(1)
   def pop
+    @store.length -= 1
+    popped = @store.pop
+    popped
   end
 
   # O(1) ammortized; O(n) worst case. Variable because of the possible
   # resize.
   def push(val)
+    self.resize! if @length == @capacity
+    @store.length
+    @store.push(val)
   end
 
   # O(n): has to shift over all the elements.
@@ -36,9 +47,12 @@ class DynamicArray
   attr_writer :length
 
   def check_index(index)
+    raise 'index out of bounds' if index > @length
+    @store[index]
   end
 
   # O(n): has to copy over all the elements to the new store.
   def resize!
+    @capacity *= 2
   end
 end
--- arrays/project/firstname_lastname/lib/static_array.rb
+++ arrays/project/firstname_lastname/lib/static_array.rb
@@ -1,14 +1,18 @@
 # This class just dumbs down a regular Array to be statically sized.
 class StaticArray
-  def initialize(length)
+  def initialize(length = 0)
+    @store = []
+    @length = length
   end
 
   # O(1)
   def [](index)
+    @store[index]
   end
 
   # O(1)
   def []=(index, value)
+    @store[index] = value;
   end
 
   protected
